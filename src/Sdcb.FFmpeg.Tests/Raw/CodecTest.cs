ec51281f2021b16361fb8b08fdc46e5903428525
sdcb <sdflysha@qq.com>
2022-06-16 14:55:50 +0800

    Update github actions.

---
+++ src/Sdcb.FFmpeg.Tests/Raw/CodecTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Sdcb.FFmpeg.Raw;
+
+namespace Sdcb.FFmpeg.Tests.Raw
+{
+    [TestFixture]
+    public class CodecTest
+    {
+        [Test]
+        public void Test_avcodec_version()
+        {
+            uint version = ffmpeg.avcodec_version();
+            uint major = version >> 16;
+            major.Should().Be(59);
+        }
+
+        [Test]
+        public void Test_av_version()
+        {
+            string version = ffmpeg.av_version_info();
+            version.Should().NotBeNull();
+        }
+    }
+}
