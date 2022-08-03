8e0490d0ae12f499c721355170c78c3631832c0d
sdcb <sdflysha@qq.com>
2022-08-03 18:01:38 +0800

    Add FFmpegLogger.

---
+++ src/Sdcb.FFmpeg.Tests/Common/LogWriterTest.cs
@@ -0,0 +1,25 @@
+﻿using Sdcb.FFmpeg.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Sdcb.FFmpeg.Tests.Common
+{
+    public class LogWriterTest
+    {
+        private readonly ITestOutputHelper _console;
+
+        public LogWriterTest(ITestOutputHelper console)
+        {
+            _console = console;
+        }
+
+        [Fact]
+        public void WriteLog()
+        {
+            string lastLog = null;
+            FFmpegLogger.LogWriter = log => lastLog = log;
+            FFmpegLogger.LogError("hello");
+            _console.WriteLine(lastLog);
+        }
+    }
+}
