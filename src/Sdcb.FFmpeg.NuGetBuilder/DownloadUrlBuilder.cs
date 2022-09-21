489ca6d31a11e07770d13171f9a70f87ca13f800
sdcb <sdflysha@qq.com>
2022-09-21 14:03:57 +0800

    bump version to 5.1.1

--- src/Sdcb.FFmpeg.NuGetBuilder/DownloadUrlBuilder.cs
+++ src/Sdcb.FFmpeg.NuGetBuilder/DownloadUrlBuilder.cs
@@ -8,7 +8,7 @@ namespace Sdcb.FFmpeg.NuGetBuilder
 {
     public static class DownloadUrlBuilder
     {
-        public const string DownloadVersion = "5.1";
+        public const string DownloadVersion = "5.1.1";
 
         public const string Url = $"https://github.com/GyanD/codexffmpeg/releases/download/{DownloadVersion}/ffmpeg-{DownloadVersion}-full_build-shared.zip";
     }
