f1ab1fb5c0939401d542c02139cae159fac4cd54
sdcb <sdflysha@qq.com>
2022-10-21 19:12:04 +0800

    Road to 4.4.3

---
+++ src/Sdcb.FFmpeg.NuGetBuilder/PackageInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Sdcb.FFmpeg.NuGetBuilder;
+
+public static class PackageInfo
+{
+    public const string Version = "4.4.3";
+
+    public const string Url = $"https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2022-10-20-12-36/ffmpeg-n4.4.3-win64-gpl-shared-4.4.zip";
+}
