9b0dd9f3e37d3a1eaa23f36f4f71db4f9f97026c
sdcb <sdflysha@qq.com>
2022-10-21 11:22:12 +0800

    Add 2 CodecExtensions to negociate.

---
+++ src/Sdcb.FFmpeg/Toolboxs/Extensions/CodecExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Sdcb.FFmpeg.Codecs;
+using Sdcb.FFmpeg.Raw;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sdcb.FFmpeg.Toolboxs.Extensions;
+
+public static class CodecExtensions
+{
+    public static AVSampleFormat NegociateSampleFormat(this Codec codec, AVSampleFormat suggestedSampleFormat) => 
+        NegociateOption(codec.SampleFormats, suggestedSampleFormat);
+
+    public static int NegociateSampleRates(this Codec codec, int suggestedSampleRate) =>
+        NegociateOption(codec.SupportedSamplerates, suggestedSampleRate);
+
+    private static T NegociateOption<T>(IEnumerable<T> source, T suggestedOption) => source.ToList() switch
+    {
+        [] => suggestedOption,
+        var x when x.Contains(suggestedOption) => suggestedOption,
+        [var x, ..] => x,
+    };
+}
