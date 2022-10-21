4f8562db44d317ab9634c846ada8346e21d44d93
sdcb <sdflysha@qq.com>
2022-10-21 11:51:11 +0800

    Add PtsDtsExtensions.

---
+++ src/Sdcb.FFmpeg/Toolboxs/Extensions/PtsDtsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Sdcb.FFmpeg.Codecs;
+using Sdcb.FFmpeg.Raw;
+using System.Collections.Generic;
+
+namespace Sdcb.FFmpeg.Toolboxs.Extensions
+{
+    public record struct PtsDts(long Pts, long Dts)
+    {
+        public bool HasValue => this != Default;
+        public static PtsDts Default => new (ffmpeg.AV_NOPTS_VALUE, ffmpeg.AV_NOPTS_VALUE);
+    }
+
+    public static class PtsDtsExtensions
+    {
+        public static IEnumerable<Packet> RecordPtsDts(this IEnumerable<Packet> packets, Dictionary<int, PtsDts> packetTiming)
+        {
+            foreach (Packet packet in packets)
+            {
+                packetTiming[packet.StreamIndex] = new(packet.Pts, packet.Dts);
+                yield return packet;
+            }
+        }
+    }
+}
