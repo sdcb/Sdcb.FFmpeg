11b6aee57b90df8cc4d065777fd0cacf0458a3d4
sdcb <sdflysha@qq.com>
2022-11-02 18:00:13 +0800

    use correct version.

--- src/Sdcb.FFmpeg.NuGetBuilder/PackageInfo.cs
+++ src/Sdcb.FFmpeg.NuGetBuilder/PackageInfo.cs
@@ -2,7 +2,7 @@
 
 public static class PackageInfo
 {
-    public const string Version = "4.4.3";
+    public const string Version = "5.1.2";
 
-    public const string Url = $"https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2022-10-20-12-36/ffmpeg-n4.4.3-win64-gpl-shared-4.4.zip";
+    public const string Url = $"https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2022-11-01-12-40/ffmpeg-n5.1.2-4-gf5455889fd-win64-gpl-shared-5.1.zip";
 }
