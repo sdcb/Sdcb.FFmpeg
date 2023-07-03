6876a99308b8014fe6c15f5095fb28d65a5f7eef
sdcb <sdflysha@qq.com>
2023-07-03 22:02:02 +0800

    Bump version to latest.

--- src/Sdcb.FFmpeg.NuGetBuilder/PackageInfo.cs
+++ src/Sdcb.FFmpeg.NuGetBuilder/PackageInfo.cs
@@ -2,7 +2,7 @@
 
 public static class PackageInfo
 {
-    public const string Version = "6.0.0-preview.1";
+    public const string Version = "6.0.26";
 
-    public const string Url = $"https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2023-02-28-12-37/ffmpeg-N-109937-g05438db024-win64-gpl-shared.zip";
+    public const string Url = $"https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2023-07-03-13-08/ffmpeg-n6.0-26-g3f345ebf21-win64-gpl-shared-6.0.zip";
 }
